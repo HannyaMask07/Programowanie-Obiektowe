e5342d301462474b878e4dd389be118a48b710f1
Krzysztof Trytek <Krzysztof.Trytek@microsoft.wsei.edu.pl>
2022-03-10 13:20:18 +0100

    Konstruktory. Metoda ToString().Przeładowanie operatorów.

---
+++ lab01/Lab01/Lab01/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    public class Ułamek
+    {
+        private int licznik;
+        private int mianownik;
+
+        public Ułamek()
+        {
+            this.licznik = 5;
+            this.mianownik = 1;
+        }
+
+        public Ułamek(int licznik, int mianownik)
+        {
+            licznik = this.licznik;
+            mianownik = this.mianownik;
+        }
+
+        public Ułamek(Ułamek ułamek)
+        {
+            this.licznik = ułamek.licznik;
+            this.mianownik = ułamek.mianownik;
+        }
+
+        public static Ułamek operator +(Ułamek a) => a;
+
+        public static Ułamek operator -(Ułamek a) => new Ułamek(-a.licznik, a.mianownik);
+
+        public static Ułamek operator +(Ułamek a, Ułamek b)
+            => new Ułamek(a.licznik * b.mianownik + b.licznik * a.mianownik, a.mianownik * b.mianownik);
+
+        public static Ułamek operator -(Ułamek a, Ułamek b)
+            => a + (-b);
+
+        public static Ułamek operator *(Ułamek a, Ułamek b)
+            => new Ułamek(a.licznik * b.licznik, a.mianownik * b.mianownik);
+
+        public static Ułamek operator /(Ułamek a, Ułamek b)
+        {
+            if (b.licznik == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Ułamek(a.licznik * b.mianownik, a.mianownik * b.licznik);
+        }
+
+        public override string ToString() => $"{licznik} / {mianownik}";
+    }
+
+}
--- lab01/Lab01/Lab01/Program.cs
+++ lab01/Lab01/Lab01/Program.cs
@@ -4,9 +4,15 @@ namespace Lab01
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var a = new Ułamek(5, 4);
+            var b = new Ułamek(1, 2);
+            Console.WriteLine(-a);   // output: -5 / 4
+            Console.WriteLine(a + b);  // output: 14 / 8
+            Console.WriteLine(a - b);  // output: 6 / 8
+            Console.WriteLine(a * b);  // output: 5 / 8
+            Console.WriteLine(a / b);  // output: 10 / 4
         }
     }
 }

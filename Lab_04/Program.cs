9e245a1a8642b72f4334b100524a0ea85e73fc12
Krzysztof Trytek <Krzysztof.Trytek@microsoft.wsei.edu.pl>
2022-04-29 12:17:29 +0200

    initial commit

---
+++ Lab_04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        DynamicArray<int> array = new DynamicArray<int>();
+
+        array[1] = 3;
+        array[5] = 4; 
+    
+
+    }
+}
+
+class DynamicArray<T>
+{
+    private T[] items = new T[0];
+
+    public T this[int i]
+    {
+
+        get { return this.items[i];  }
+        set
+        {
+            if (i > this.items.Length)
+            {
+                T[] newItems = new T[i + 1];
+
+                Array.Copy(this.items, newItems, this.items.Length);
+            }
+
+            this.items[i] = value;
+        }
+
+    }
+}
---
+++ Lab_06/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>()
+            {
+                new User {Name = "A", Age = 1},
+                new User {Name = "B", Age = 2},
+                new User {Name = "C", Age = 3},
+                new User {Name = "A", Age = 4},
+            };
+
+            Console.WriteLine(users.Count());
+            
+            Console.WriteLine((from user  in users
+                               select user).Count());
+
+
+            List<string> names_1 = users.Select(user => user.Name).ToList(); //mapowania, projekcja
+            List<string> names_2 = (from user in users
+                                    select user.Name).ToList(); // mapowania projekcja
+
+            //foreach (string name in names_2)
+            //    Console.WriteLine(name);
+
+            List<User> users_1 = users.OrderBy(user => user.Name).ToList();
+            List<User> users_2 = (from user in users
+                                  orderby user.Name
+                                  select user).ToList();
+
+            foreach (User user in users_1)
+                Console.WriteLine(user.Name + " " + user.Age);
+        }
+    }
+}
